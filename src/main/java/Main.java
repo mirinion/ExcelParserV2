52ff1a42e7452bd3c4f1c2abf1243fd007674482
mirinion <mirinion@bk.ru>
2022-10-25 19:25:19 +0300

    fixed theme num

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,10 @@ import java.util.ArrayList;
 public class Main {
     private static final String SOURCE_XLSX = "src/main/resources/source.xlsx";
     private static final String TARGET_TXT = "src/main/resources/target.txt";
-    private static final int THEME_NUM = 4;
+    private static final int THEME_NUM = 1;
 
     public static void main(String[] args) throws IOException {
         ArrayList<Quest> quests = XLSXAnalyzer.createQuestsList(SOURCE_XLSX, THEME_NUM);
         HTMLFiller.fill(TARGET_TXT, quests);
-
-
     }
 }
