87d01a4ef877e3af01baf74558609a046b3f16dc
mirinion <mirinion@bk.ru>
2022-10-25 19:39:21 +0300

    fixed theme num

--- src/main/java/XLSXAnalyzer.java
+++ src/main/java/XLSXAnalyzer.java
@@ -13,7 +13,7 @@ public class XLSXAnalyzer {
 
     public static ArrayList<Quest> createQuestsList(String sourceFile, int themeNumber) throws IOException {
         ArrayList<Quest> quests = new ArrayList<>();
-        XSSFSheet sheet = getSheet(sourceFile, "Sheet1");
+        XSSFSheet sheet = getSheet(sourceFile);
 
         //Алгоритм упрощен, работает только если 4 варианта ответа
         int rowCnt0to4 = 0; //Считает строки от 0 до 4 и сбрасывается. 0 - текст вопроса, 1-4 - текст вариантов ответа
@@ -30,9 +30,10 @@ public class XLSXAnalyzer {
         return quests;
     }
 
-    private static XSSFSheet getSheet(String sourceFile, String sheetName) throws IOException {
-        XSSFWorkbook wb = new XSSFWorkbook(sourceFile);
-        return wb.getSheet(sheetName);
+    private static XSSFSheet getSheet(String sourceFile) throws IOException {
+        try (XSSFWorkbook wb = new XSSFWorkbook(sourceFile)) {
+            return wb.getSheetAt(0);
+        }
     }
 
     private static boolean analyzeRow(Row row, ArrayList<Quest> quests, int rowCnt0to4, int themeNumber) {
@@ -42,19 +43,25 @@ public class XLSXAnalyzer {
             return false;
         }
         Cell cell1 = row.getCell(1);
-        if(isQuest(rowCnt0to4)) { //Если в строке записана формулировка вопроса
-            Quest quest = new Quest(themeNumber);
-            quest.setQuestNum(parseQuestNum(cell0.getStringCellValue()));
-            quest.setQuestText(parseQuestText(cell0.getStringCellValue()));
-            quest.setCorrectAns(cell1.getStringCellValue());
-            quests.add(quest);
+        try {
+            if(isQuest(rowCnt0to4)) { //Если в строке записана формулировка вопроса
+                Quest quest = new Quest(themeNumber);
+                quest.setQuestNum(parseQuestNum(cell0.getStringCellValue()));
+                quest.setQuestText(parseQuestText(cell0.getStringCellValue()));
+                quest.setCorrectAns(cell1.getStringCellValue());
+                quests.add(quest);
 
-        } else { //Если в строке записан вариант ответа
-            Quest quest = quests.get(quests.size()-1); //Последний записанный вопрос
-            String answerText = parseAnswerText(cell0.getStringCellValue(), quest.getQuestNum());
-            String answerLetter = parseAnswerLetter(cell0.getStringCellValue());
-            quest.addAnswer(answerText, answerLetter);
+            } else { //Если в строке записан вариант ответа
+                Quest quest = quests.get(quests.size()-1); //Последний записанный вопрос
+                String answerText = parseAnswerText(cell0.getStringCellValue());
+                String answerLetter = parseAnswerLetter(cell0.getStringCellValue());
+                quest.addAnswer(answerText, answerLetter);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.err.println("\n in Source file Row number " + row.getRowNum());
         }
+
         return true;
     }
 
@@ -65,31 +72,15 @@ public class XLSXAnalyzer {
     }
 
     private static int parseQuestNum(String questWholeStr){
-        try {
-            return Integer.parseInt(questWholeStr.trim().split("\\.", 2)[0]);
-        } catch (Exception exception){
-            System.err.println(exception + " in quest" + questWholeStr);
-        }
-        return -1;
+        return Integer.parseInt(questWholeStr.trim().split("\\.", 2)[0]);
     }
 
     private static String parseQuestText(String questWholeStr) {
-        try {
-            return questWholeStr.split("\\.", 2)[1];
-        } catch (Exception exception){
-            System.err.println(exception + " in quest" + questWholeStr);
-        }
-        return "";
+        return questWholeStr.split("\\.", 2)[1];
     }
 
-    private static String parseAnswerText(String ansWholeStr, int questNum) {
-        try {
-            return ansWholeStr.trim().split( "\\)", 2)[1];
-        } catch (Exception exception){
-            System.err.println(exception + " in ans" + ansWholeStr + "\n" + "questNum " + questNum);
-        }
-        return "";
-
+    private static String parseAnswerText(String ansWholeStr) {
+        return ansWholeStr.trim().split( "\\)", 2)[1];
     }
 
     private static String parseAnswerLetter(String ansWholeStr) {
